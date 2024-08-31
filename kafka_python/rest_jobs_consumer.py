07b16727e3a5f5a18dbab4becaa4e355f17d4168
alin2025 <alin.perzov@gmail.com>
2024-08-31 20:15:04 +0000

    a

---
+++ kafka_python/rest_jobs_consumer.py
@@ -0,0 +1,14 @@
+from kafka import KafkaConsumer
+
+# In this example we will illustrate a simple producer-consumer integration
+
+bootstrapServers = "course-kafka:9092"
+topic1 = 'From_API_To_Kafka_job'
+
+# First we set the consumer,
+consumer = KafkaConsumer(topic1,bootstrap_servers=bootstrapServers)
+
+# print the value of the consumer
+# we run the consumer to fetch the message scoming from topic1.
+for message in consumer:
+    print(str(message.value))
